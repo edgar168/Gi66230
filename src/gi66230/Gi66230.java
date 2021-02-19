65221d73a3be37592bfc95d12ac6b0ee72152655
EDGAR AIMAR <EDGAR AIMAR@DESKTOP-GRP7BKA>
2021-02-19 15:56:48 -0600

    Pimer commit

---
+++ src/gi66230/Gi66230.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package gi66230;
+
+/**
+ *
+ * @author EDGAR AIMAR
+ */
+public class Gi66230 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+String nombre = "Edgar Aimar";
+String matricula = "66230";
+
+System.out.println("Mi nombre es " + nombre + " y mi matricula es " + matricula);
+
+int edad = 18;
+    }
+
+}
